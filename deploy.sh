4ce0a3684745671f1af514b4943b39afde3373a1
Carter Clackson <clacksoncarter@gmail.com>
2023-12-22 09:03:06 -0600

    Update deploy to be more explicit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,26 @@
+# Change directory to the server folder
 cd server
 
+# Install Node.js dependencies
 npm install
 
-npm start
+# Go back to the root directory
+cd ..
+
+# Create a temporary directory for deployment
+deploy_dir=$(mktemp -d)
+
+# Copy only the necessary files for the server to the deployment directory
+cp -r server/* $deploy_dir
+
+# Create a zip file for deployment
+zip -r release.zip -j $deploy_dir
+
+# Clean up the temporary deployment directory
+rm -rf $deploy_dir
+
+# Print the contents of the zip file (for verification, optional)
+unzip -l release.zip
+
+# Start the deployment by copying the zip file to the deployment directory
+echo "Deploying..."
