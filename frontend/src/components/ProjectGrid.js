92025f653929b8381c5897dd5dc7afca561f5e78
Carter Clackson <clacksoncarter@gmail.com>
2024-03-25 17:31:14 -0500

    Update projects

--- frontend/src/components/ProjectGrid.js
+++ frontend/src/components/ProjectGrid.js
@@ -23,14 +23,6 @@ const ProjectGrid = () => {
 			techUsed: ['php', 'javascript'],
 		},
 		{
-			title: 'Portfolio',
-			description:
-				'This project serves as my personal portfolio, showcasing a collection of my work, skills, and experiences. It provides an interactive and visually appealing platform to highlight various projects, technologies, and achievements. The source code for this portfolio is available on GitHub, accessible through the link below.',
-			link: 'https://github.com/CarterClackson/portfolio',
-			imageSrc: 'assets/images/portfolio.PNG',
-			techUsed: ['mongodb', 'javascript', 'react', 'nodejs'],
-		},
-		{
 			title: 'equityEyes',
 			description:
 				'This project is a dynamic stock management application that allows users to search, save, and analyze stocks within their own personalized accounts. This platform offers daily updates, personalized data analysis, and a user-friendly interface for informed investment decisions. The source code for this project is available on Github, accessible through the link below.',
